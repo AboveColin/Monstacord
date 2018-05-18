7bc7aa190f85b4cf87a5db81360fa34864fca8b9
Silent-Trainer <39207164+Silent-Trainer@users.noreply.github.com>
2018-05-18 19:47:25 -0400

    adding the +help command

--- commands/help.js
+++ commands/help.js
@@ -1,42 +1,23 @@
-const { prefix } = require('../config.json');
+const Discord = require('discord.js');
 
 module.exports = {
 	name: 'help',
-	description: 'List all of my commands or info about a specific command.',
-	aliases: ['commands'],
-	usage: '[command name]',
+	description: 'Get info on how to obtain your first pokemon',
 	cooldown: 5,
-	execute(message, args) {
-		const { commands } = message.client;
-		const data = [];
+	execute(message) {
+		  const exampleEmbed = new Discord.RichEmbed()
+      .setColor('#0099ff')
+      .setTitle('Help')
+      .setURL('https://discord.gg/jjw8n82')
+      .setAuthor('Monsta-Madness', 'https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048', 'https://cdn.discordapp.com/attachments/446131041447575590/447152580452155393/200px-Ashachu.png')
+      .setDescription('Welcome to Monstacoord! Commands at your fingertips!')
+      .setThumbnail('https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048')
+      .addField('Commands', '**__Getting Started__**\n+start | Start-up Message\n+choose | Pick Your Pokemon\n+help | Shows This Message\n**__Pokemon Commands__**\n+select | Selecting a Pokemon\n+info | Info on the Current Selected Pokemon\n+party | Shows your Party List\n+mega | Mega Pokemon!\n+catch | Catch a Spawned Pokemon\n+pokemon | Shows a List of Your Pokemon\n+duel | Duel With Someone\n+trade | Trade With Someone')
+      .addBlankField()
+      .setImage('https://cdn.discordapp.com/attachments/446131041447575590/447154914792046621/starters.png')
+      .setTimestamp()
+      .setFooter('Again thanks for chooseing Monstacord', 'https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048');
 
-		if (!args.length) {
-			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
-		}
-		else {
-			if (!commands.has(args[0])) {
-				return message.reply('that\'s not a valid command!');
-			}
-
-			const command = commands.get(args[0]);
-
-			data.push(`**Name:** ${command.name}`);
-
-			if (command.description) data.push(`**Description:** ${command.description}`);
-			if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-			if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-			data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
-		}
-
-		message.author.send(data, { split: true })
-			.then(() => {
-				if (message.channel.type !== 'dm') {
-					message.channel.send('I\'ve sent you a DM with all my commands!');
-				}
-			})
-			.catch(() => message.reply('it seems like I can\'t DM you!'));
-	},
+      message.channel.send({ embed: exampleEmbed });
+  },
 };
