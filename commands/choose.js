b5d2f5daeb62705dd9a6d08b7747f4f6455f987c
Silent-Trainer <39207164+Silent-Trainer@users.noreply.github.com>
2018-05-20 18:31:51 -0400

    choose.js now has 1 arg but no api

--- commands/choose.js
+++ commands/choose.js
@@ -1,20 +1,12 @@
 module.exports = {
 	name: 'choose',
-	description: 'Ping!',
-	cooldown: 5,
+	description: 'Choose A Starter Pokemon',
+	cooldown: 10,
 	execute(message, args) {
-		const pokemon = parseInt(args[0]) + 1;
-
-		if (isNaN(pokemon)) {
-			return message.reply('that doesn\'t seem to be a valid starter pokemon name.');
-		}
-		else if (pokemon <= bulbasaur || amount > litten) {
-			return message.reply('you need to input a starter name to get started.');
+		if (args[0] === 'bulbasaur') {
+			return message.channel.send(`Good Choice ${message.author}! Bulbasaur is now your first Pokemon!`);
 		}
 
-		message.channel.bulkDelete(pokemon, true).catch(err => {
-			console.error(err);
-			message.channel.send('there was an error trying to get your starter pokemon!');
-		});
+		message.channel.send(`First argument: ${args[0]}`);
 	},
 };
