742ea123d31f476fca31fb231179da6ab05858b3
Silent-Trainer <39207164+Silent-Trainer@users.noreply.github.com>
2018-05-20 18:38:59 -0400

    preparing args

--- commands/daycare.js
+++ commands/daycare.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'daycare',
 	description: 'Let us take your Pokemon to Lvl 100',
 	cooldown: 5,
-	execute(message) {
+	execute(message, args) {
 		message.channel.sendMessage('`Coming Soon`');
 	},
 };
