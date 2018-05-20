9f982d76b2ac3575069d795f7a1bf9c1beccb50c
Silent-Trainer <39207164+Silent-Trainer@users.noreply.github.com>
2018-05-20 18:29:46 -0400

    updated botinfo.js

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -4,11 +4,18 @@ module.exports = {
 	name: 'botinfo',
 	description: 'Display info about yourself.',
 	execute(message) {
-        const embed = new Discord.RichEmbed()
-		.setDiscription("Bot Information")
-		.setColor("#008000")
-		.addField("Bot Name", bot.user.username);
-		
-		message.channel.send({embed});
+		const exampleEmbed = new Discord.RichEmbed()
+	    .setColor('#0099ff')
+	    .setTitle('Monstacord')
+	    .setURL('https://discord.gg/jjw8n82')
+	    .setAuthor('Monsta-Madness', 'https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048', 'https://cdn.discordapp.com/attachments/446131041447575590/447152580452155393/200px-Ashachu.png')
+	    .setDescription('Check out all the info here')
+	    .setThumbnail('https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048')
+	    .addField('Information', 'This bot is for Discord and Pokemon alike. Todays best bot around is Pokecord until Monstacord was released. We plan on a release in early August. More info coming soon!')
+	    .addBlankField()
+	    .setTimestamp()
+	    .setFooter('You can always DM ༒TᴀͪsͤʜᴋᴇᴛᴄʜͬᴜͤᴍͣL ™༒ if you would like to help with this project.', 'https://cdn.discordapp.com/avatars/236856961478557696/e4755472dcea6b2edc70194713487faa.png?size=2048');
+
+	    message.channel.send({ embed: exampleEmbed });
     },
-};		
+};	
