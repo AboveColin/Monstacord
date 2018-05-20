41d23c8bb507c53a2dae24fabe0448a01525b802
Silent-Trainer <39207164+Silent-Trainer@users.noreply.github.com>
2018-05-20 18:38:17 -0400

    Update daycare.js

--- commands/daycare.js
+++ commands/daycare.js
@@ -3,6 +3,6 @@ module.exports = {
 	description: 'Let us take your Pokemon to Lvl 100',
 	cooldown: 5,
 	execute(message) {
-		message.channel.sendMessage('COMING SOON');
+		message.channel.sendMessage('`Coming Soon`');
 	},
 };
